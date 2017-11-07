b468dbb2201594f4dd36572617288c7cd2dc7565
matheeuscb <matheus.cb@hotmail.com>
2017-11-07 12:50:41 -0200

    versao6

--- Mosaico/wsgi.py
+++ Mosaico/wsgi.py
@@ -13,5 +13,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mosaico.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+try:
+	from whitenoise.django import DjangoWhiteNoise
+	application = DjangoWhiteNoise(application)
+except:
+	pass
